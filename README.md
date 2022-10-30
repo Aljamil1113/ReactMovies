5b98b1cc78176a3273a457c489245965e9fbb6a7
Aljamil1113 <aarazasaljamil@gmail.com>
2022-10-30 23:32:02 +0800

    Client: Genre CRUD and other pkg

--- README.md
+++ README.md
@@ -29,3 +29,5 @@ npm i react-bootstrap-typeahead@5.2.2
 npm i -D @types/react-bootstrap-typeahead@5.1.8
 
 npm i axios@0.21.1
+
+npm i sweetalert2@10.16.0
--- react-movies/src/genres/IndexGenres.tsx
+++ react-movies/src/genres/IndexGenres.tsx
@@ -1,22 +1,88 @@
 import axios, { AxiosResponse } from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { urlGenres } from "../endpoints";
+import Button from "../utils/Button";
+import customConfirm from "../utils/customConfirm";
+import GenericList from "../utils/GenericList";
+import Pagination from "../utils/Pagination";
+import RecordsPerPageSelect from "../utils/RecordsPerPageSelect";
 import { genreDto } from "./genre.model";
 
 export default function IndexGenres() {
 
+    const [genres, setGenres] = useState<genreDto[]>();
+    const [totalAmountOfPages, setTotalAmountOfPages] = useState(0);
+    const [recordsPerPage, setRecordsPerPage] = useState(10);
+    const [page, setPage] = useState(1);
+
     useEffect(() => {
-        axios.get(urlGenres)
+        loadData();
+    }, [page, recordsPerPage])
+
+    function loadData() {
+        axios.get(urlGenres, {
+            params: {page, recordsPerPage}
+        })
             .then((response: AxiosResponse<genreDto[]>) => {
-                console.log(response.data);
+               const totalAmountOfRecords = parseInt(response.headers['totalamountofrecords'], 10);
+               setTotalAmountOfPages(Math.ceil(totalAmountOfRecords / recordsPerPage));
+               setGenres(response.data);
             })
-    })
+    }
+
+    async function deleteGenre(id: number) {
+        try{
+            await axios.delete(`${urlGenres}/${id}`);
+            loadData();
+        } 
+        catch(error: any) {
+            if(error && error.response) {
+                console.log(error(error.response.data));
+            }
+        }
+    }
 
     return (
         <>
             <h3>Genres</h3>
             <Link className="btn btn-primary" to="/genres/create">Create Genre</Link>
+
+            <RecordsPerPageSelect onChange={amountOfRecords => {
+                setPage(1);
+                setRecordsPerPage(amountOfRecords)
+            }}/>
+
+            <Pagination currentPage={page} totalAmountOfPages={totalAmountOfPages}
+                onChange={newPage => setPage(newPage)} />
+
+            <GenericList list={genres}>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                        </tr>                       
+                    </thead>
+                    <tbody>
+                        {genres?.map(genre => 
+                            <tr key={genre.id}>
+                                <td>
+                                    <Link className="btn btn-success" to={`/genres/edit/${genre.id}`}>Edit</Link>
+
+                                    <Button 
+                                    onClick={() => customConfirm(() => deleteGenre(genre.id))}
+                                    className="btn btn-danger">Delete</Button>
+                                </td>
+
+                                <td>
+                                    {genre.name}
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </GenericList>
         </>
     )
 }
